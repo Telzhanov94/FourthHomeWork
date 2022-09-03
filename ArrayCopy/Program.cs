986c2c034ef4bd9f3c1c798716f5d1072e955e6f
Temirlan <telzhanov94@gmail.com>
2022-09-03 22:45:32 +0600

    ready

---
+++ ArrayCopy/Program.cs
@@ -0,0 +1,30 @@
+﻿int [] CreateArray(int size, int min, int max)
+{
+    int [] array = new int [size];
+    for (int i=0;i<array.Length;i++)
+    array[i]=new Random().Next (min,max+1);
+    return array;
+}
+void PrintArray(int[]array)
+{
+    foreach(var element in array)
+    Console.Write($"{element} ");
+}
+int [] CopyArray(int [] array)
+{
+    int [] NewArray = new int [array.Length];
+    for (int i=0;i<array.Length;i++)
+    NewArray[i]=array[i];
+    return NewArray;
+}
+Console.Write("Enter size in array: ");
+int size = int.Parse(Console.ReadLine()??"0");
+Console.Write("Enter minimum value: ");
+int min = int.Parse(Console.ReadLine()??"0");
+Console.Write("Enter maximum value: ");
+int max = int.Parse(Console.ReadLine()??"0");
+int [] Array = CreateArray(size,min,max);
+PrintArray(Array);
+Console.WriteLine();
+int [] NewArray = CopyArray(Array);
+PrintArray(NewArray);
