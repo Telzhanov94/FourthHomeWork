b0512e40085153093a576c587d20be88a8be83dd
Temirlan <telzhanov94@gmail.com>
2022-09-05 15:23:34 +0600

    Доработал

--- Coordinate/Program.cs
+++ Coordinate/Program.cs
@@ -1,7 +1,14 @@
-﻿int k1=1;
-int k2=2;
-int b1=3;
-int b2=4;
+﻿Console.WriteLine ("Enter a variable k1");
+int k1=int.Parse (Console.ReadLine()??"0");
+Console.WriteLine ("Enter a variable b1");
+int b1=int.Parse (Console.ReadLine()??"0");
+Console.WriteLine ("Enter a variable k2");
+int k2=int.Parse (Console.ReadLine()??"0");
+Console.WriteLine ("Enter a variable b2");
+int b2=int.Parse (Console.ReadLine()??"0");
+if (k1==k2 & b1==b2) {Console.WriteLine("The functions match");}
+else
+{
 for (int x=-100; x<100;x++)
 {
     int y1 =k1*x+b1;
@@ -11,5 +18,7 @@ for (int x=-100; x<100;x++)
     Console.Write ("Intersections of given lines at a point" + x +" " + y1 ); 
     Console.WriteLine();
    }
-   else {Console.WriteLine ("Given straight parallel");}
+   else 
+   {Console.WriteLine ("Given straight parallel");break;}
+}
 }
